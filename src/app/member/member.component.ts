3825c20f909a1157bcbafd22854d5233e792d524
Monique St. Laurent <moinstla@gmail.com>
2017-06-16 15:13:27 -0700

    update member component

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
-  styleUrls: ['./member.component.css']
+  styleUrls: ['./member.component.css'],
+  providers: [MemberService]
 })
 export class MemberComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
